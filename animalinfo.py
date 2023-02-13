88c1e2c2ea6ab39d2a3e31c0a3cdfbd630c4e5ed
Leonardo Ledda <leonardoledda@gmail.com>
2023-02-13 14:27:56 +0100

    animalinfo: Format with yapf
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- animalinfo.py
+++ animalinfo.py
@@ -20,8 +20,8 @@ load_dotenv()
 key = os.getenv("API_KEY")
 
 # Gets the current commit hash from GitHub
-gh = github3.login(token = os.getenv("GITHUB_TOKEN"))
-repository = gh.repository(owner = "LeddaZ", repository = "animalinfo")
+gh = github3.login(token=os.getenv("GITHUB_TOKEN"))
+repository = gh.repository(owner="LeddaZ", repository="animalinfo")
 lastCommitHash = repository.branch("main").latest_sha()
 
 # Trains the ML model to recognize images
@@ -34,22 +34,24 @@ window.title("Animal Info " + lastCommitHash[:8])
 window.resizable(False, False)
 
 # Fonts
-titleFont = TkFont.Font(family = "helvetica", size = 36, weight = "bold")
-animalFont = TkFont.Font(family = "helvetica", size = 20, weight = "bold")
-descFont = TkFont.Font(family = "helvetica", size = 14)
+titleFont = TkFont.Font(family="helvetica", size=36, weight="bold")
+animalFont = TkFont.Font(family="helvetica", size=20, weight="bold")
+descFont = TkFont.Font(family="helvetica", size=14)
 
 # Title
-titleLabel = tk.Label(window, text = "Animal Info", font = titleFont)
-titleLabel.grid(row = 1, column = 1)
+titleLabel = tk.Label(window, text="Animal Info", font=titleFont)
+titleLabel.grid(row=1, column=1)
 
 # Image chooser button
-button = tk.Button(window, text = "Choose an image", command = lambda:recognize_image())
-button.grid(row = 2, column = 1, pady = 5)
+button = tk.Button(window,
+                   text="Choose an image",
+                   command=lambda: recognize_image())
+button.grid(row=2, column=1, pady=5)
 
 # Language selector
 combo_value = tk.StringVar()
 combobox = ttk.Combobox(window, textvariable=combo_value)
-combobox.grid(row = 3, column = 1, pady = 5)
+combobox.grid(row=3, column=1, pady=5)
 combobox["values"] = ("English", "Italiano")
 combobox["state"] = "readonly"
 combobox.current(0)
@@ -57,7 +59,10 @@ combobox.current(0)
 # Language-dependent data
 wiki_languages = ["en", "it"]
 page_titles_en = ["Dog", "Cat", "Capybara", "Frog", "Rabbit", "Pig", "Snake"]
-page_titles_it = ["Cane", "Gatto", "Capibara", "Rana", "Coniglio", "Maiale", "Serpente"]
+page_titles_it = [
+    "Cane", "Gatto", "Capibara", "Rana", "Coniglio", "Maiale", "Serpente"
+]
+
 
 # Chooses an image to pass to the ML model, gets the result and displays the
 # appropriate image and Wikipedia description
@@ -65,63 +70,73 @@ def recognize_image():
     global img
     global animalLabel
     try:
-        animalLabel.config(text = " ")
+        animalLabel.config(text=" ")
     except NameError:
         pass
     f_types = [("Image files", "*.bmp *.jpg *.jpeg *.png")]
-    filename = filedialog.askopenfilename(filetypes = f_types)
+    filename = filedialog.askopenfilename(filetypes=f_types)
 
     demo = detector.prediction(filename)
     label = demo["class_name"]
     confidence = demo["confidence"]
 
-    if(label != "Other"):
-        img = ImageTk.PhotoImage(file = str.lower("assets/%s.jpg" % label))
-        imageLabel = tk.Label(window, image = img)
-        imageLabel.grid(row = 5, column = 1)
+    if (label != "Other"):
+        img = ImageTk.PhotoImage(file=str.lower("assets/%s.jpg" % label))
+        imageLabel = tk.Label(window, image=img)
+        imageLabel.grid(row=5, column=1)
 
         print("I'm %d%% sure this is a %s." % (confidence, str.lower(label)))
 
-        if(combobox.current() == 1):
-            if(label == page_titles_en[0]):
+        if (combobox.current() == 1):
+            if (label == page_titles_en[0]):
                 label = page_titles_it[0]
-            elif((label == page_titles_en[1])):
+            elif ((label == page_titles_en[1])):
                 label = page_titles_it[1]
-            elif((label == page_titles_en[2])):
+            elif ((label == page_titles_en[2])):
                 label = page_titles_it[2]
-            elif((label == page_titles_en[3])):
+            elif ((label == page_titles_en[3])):
                 label = page_titles_it[3]
-            elif((label == page_titles_en[4])):
+            elif ((label == page_titles_en[4])):
                 label = page_titles_it[4]
-            elif((label == page_titles_en[5])):
+            elif ((label == page_titles_en[5])):
                 label = page_titles_it[5]
-            elif((label == page_titles_en[6])):
+            elif ((label == page_titles_en[6])):
                 label = page_titles_it[6]
 
         wiki = wikipediaapi.Wikipedia(wiki_languages[combobox.current()])
         page = wiki.page(label)
         desc = page.summary
 
-        desc_area = scrolledtext.ScrolledText(window, wrap = tk.WORD, width = 50, height = 10, font = descFont)    
-        desc_area.grid(row = 6, column = 1, pady = 10)
+        desc_area = scrolledtext.ScrolledText(window,
+                                              wrap=tk.WORD,
+                                              width=50,
+                                              height=10,
+                                              font=descFont)
+        desc_area.grid(row=6, column=1, pady=10)
 
-        desc_area.tag_configure("tag-center", justify = "center")
+        desc_area.tag_configure("tag-center", justify="center")
         desc_area.insert(tk.END, desc, "tag-center")
 
-        desc_area.configure(state = "disabled")
+        desc_area.configure(state="disabled")
     else:
-        desc_area = scrolledtext.ScrolledText(window, wrap = tk.WORD, width = 50, height = 10, font = descFont)    
-        desc_area.grid(row = 6, column = 1, pady = 10)
+        desc_area = scrolledtext.ScrolledText(window,
+                                              wrap=tk.WORD,
+                                              width=50,
+                                              height=10,
+                                              font=descFont)
+        desc_area.grid(row=6, column=1, pady=10)
 
-        desc_area.tag_configure("tag-center", justify = "center")
-        desc_area.insert(tk.END, "This animal is not supported by the model.", "tag-center")
+        desc_area.tag_configure("tag-center", justify="center")
+        desc_area.insert(tk.END, "This animal is not supported by the model.",
+                         "tag-center")
 
-        desc_area.configure(state = "disabled")
+        desc_area.configure(state="disabled")
 
         print("I'm %d%% sure this isn't a supported animal." % (confidence))
 
-    animalLabel = tk.Label(window, text = label, font = animalFont)
-    animalLabel.grid(row = 4, column = 1)
+    animalLabel = tk.Label(window, text=label, font=animalFont)
+    animalLabel.grid(row=4, column=1)
+
 
 # Keeps the window open
 window.mainloop()
--- mlforkids.py
+++ mlforkids.py
@@ -1,6 +1,8 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+
 tf.get_logger().setLevel('ERROR')
 
 import tensorflow_hub as hub
@@ -14,14 +16,15 @@ import numpy as np
 import urllib.request, urllib.error, json
 from time import sleep
 
+
 #
 # Helper class for training an image classifier using training data
 #  from the Machine Learning for Kids website.
 #
 class MLforKidsImageProject:
 
-    IMAGESIZE=(224,224)
-    INPUTLAYERSIZE=IMAGESIZE + (3,)
+    IMAGESIZE = (224, 224)
+    INPUTLAYERSIZE = IMAGESIZE + (3, )
 
     # scratchkey is the secret API key that allows access to training
     #  data from a single project on the MLforKids website
@@ -30,90 +33,116 @@ class MLforKidsImageProject:
         opener = urllib.request.build_opener(MLforKidsHTTP())
         urllib.request.install_opener(opener)
 
-        print("MLFORKIDS: Downloading information about your machine learning project")
+        print(
+            "MLFORKIDS: Downloading information about your machine learning project"
+        )
         self.scratchkey = scratchkey
         try:
-            apiurl = self.__switchToTemporarySite("https://machinelearningforkids.co.uk/api/scratch/" + scratchkey + "/train")
+            apiurl = self.__switchToTemporarySite(
+                "https://machinelearningforkids.co.uk/api/scratch/" +
+                scratchkey + "/train")
             with urllib.request.urlopen(apiurl) as url:
-                self.__downloaded_training_images_list = json.loads(url.read().decode())
+                self.__downloaded_training_images_list = json.loads(
+                    url.read().decode())
         except urllib.error.HTTPError:
-            raise RuntimeError("Unable to retrieve machine learning project - please check that the key is correct")
+            raise RuntimeError(
+                "Unable to retrieve machine learning project - please check that the key is correct"
+            )
 
     # Generates a name for the local cache file where the downloaded training
     #  image is saved. An image file extension is required, otherwise it will
     #  be ignored by ImageDataGenerator.
     def __get_fname(self, trainingitem):
-        extension = ".png" if trainingitem["imageurl"].lower().endswith(".png") else ".jpg"
+        extension = ".png" if trainingitem["imageurl"].lower().endswith(
+            ".png") else ".jpg"
         return trainingitem["id"] + extension
 
     # Downloads all of the training images for this project, and sets up an
     #  ImageDataGenerator against the folder where they have been downloaded
     def __get_training_images_generator(self):
-        print("MLFORKIDS: Getting your training images to use to train your machine learning model")
+        print(
+            "MLFORKIDS: Getting your training images to use to train your machine learning model"
+        )
         cachedir = "~/.keras/"
         cachelocation = os.path.join("datasets", "mlforkids", self.scratchkey)
-        projectcachedir = str(os.path.expanduser(os.path.join(cachedir, cachelocation)))
+        projectcachedir = str(
+            os.path.expanduser(os.path.join(cachedir, cachelocation)))
         for trainingitem in self.__downloaded_training_images_list:
             try:
-                tf.keras.utils.get_file(origin=self.__switchToTemporarySite(trainingitem["imageurl"]),
+                tf.keras.utils.get_file(origin=self.__switchToTemporarySite(
+                    trainingitem["imageurl"]),
                                         cache_dir=cachedir,
-                                        cache_subdir=os.path.join(cachelocation, trainingitem["label"]),
+                                        cache_subdir=os.path.join(
+                                            cachelocation,
+                                            trainingitem["label"]),
                                         fname=self.__get_fname(trainingitem))
                 # avoid common rate-limiting errors by pausing
                 #  for a quarter-second between each download
                 sleep(0.25)
             except Exception as downloaderr:
-                print("ERROR: Unable to download training image from", trainingitem["imageurl"])
+                print("ERROR: Unable to download training image from",
+                      trainingitem["imageurl"])
                 print(downloaderr)
-                print("ERROR: Skipping training image and continuing without it", trainingitem["imageurl"])
-        return ImageDataGenerator().flow_from_directory(str(projectcachedir),
-                                                        target_size=MLforKidsImageProject.IMAGESIZE)
+                print(
+                    "ERROR: Skipping training image and continuing without it",
+                    trainingitem["imageurl"])
+        return ImageDataGenerator().flow_from_directory(
+            str(projectcachedir), target_size=MLforKidsImageProject.IMAGESIZE)
 
     # Creates a lookup table for the classes that this project is being trained
     #  to recognize.
     # TODO : dumb implementation - should rewrite
     def __get_class_lookup(self, training_image_data):
-        class_labels = [None]*training_image_data.num_classes
+        class_labels = [None] * training_image_data.num_classes
         class_names = training_image_data.class_indices.keys()
         for classname in class_names:
-            class_labels[training_image_data.class_indices[classname]] = classname
+            class_labels[
+                training_image_data.class_indices[classname]] = classname
         return class_labels
 
     # Defines a simple image classifier based on a mobilenet model from TensorFlow hub
     def __define_model(self):
-        print("MLFORKIDS: Defining the layers to include in your neural network")
+        print(
+            "MLFORKIDS: Defining the layers to include in your neural network")
         model = Sequential([
             # input layer is resizing all images to save having to do that in a manual pre-processing step
-            Rescaling(1/127, input_shape=MLforKidsImageProject.INPUTLAYERSIZE),
+            Rescaling(1 / 127,
+                      input_shape=MLforKidsImageProject.INPUTLAYERSIZE),
             # using an existing pre-trained model as an untrainable main layer
-            hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/5"),
+            hub.KerasLayer(
+                "https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/5"
+            ),
             #
             Dropout(rate=0.2),
             #
             Dense(self.num_classes)
         ])
-        model.build((None,) + MLforKidsImageProject.INPUTLAYERSIZE)
+        model.build((None, ) + MLforKidsImageProject.INPUTLAYERSIZE)
 
         # model compile parameters copied from tutorial at https://www.tensorflow.org/hub/tutorials/tf2_image_retraining
-        model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=0.005, momentum=0.9),
-            loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=0.1),
-            metrics=['accuracy'])
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.005,
+                                                        momentum=0.9),
+                      loss=tf.keras.losses.CategoricalCrossentropy(
+                          from_logits=True, label_smoothing=0.1),
+                      metrics=['accuracy'])
 
         return model
 
     # Runs the model fit function to train the tl model
     def __train_model(self, trainingimagesdata):
-        print("MLFORKIDS: Starting the training of your machine learning model")
+        print(
+            "MLFORKIDS: Starting the training of your machine learning model")
         if trainingimagesdata.batch_size > trainingimagesdata.samples:
             trainingimagesdata.batch_size = trainingimagesdata.samples
         steps_per_epoch = trainingimagesdata.samples // trainingimagesdata.batch_size
         epochs = 8
         if trainingimagesdata.samples > 55:
             epochs = 15
-        self.ml_model.fit(trainingimagesdata, epochs=epochs, steps_per_epoch=steps_per_epoch, verbose=0)
+        self.ml_model.fit(trainingimagesdata,
+                          epochs=epochs,
+                          steps_per_epoch=steps_per_epoch,
+                          verbose=0)
         print("MLFORKIDS: Model training complete")
-
 
     # Cloudflare is currently blocking access to the Machine Learning for Kids API
     #  from non-browser user agents
@@ -124,9 +153,10 @@ class MLforKidsImageProject:
     #  stopped breaking Python apps
     #
     def __switchToTemporarySite(self, url):
-        return url.replace("https://machinelearningforkids.co.uk/api/scratch/",
-                           "https://mlforkids-api.j8clybxvjr0.us-south.codeengine.appdomain.cloud/api/scratch/")
-
+        return url.replace(
+            "https://machinelearningforkids.co.uk/api/scratch/",
+            "https://mlforkids-api.j8clybxvjr0.us-south.codeengine.appdomain.cloud/api/scratch/"
+        )
 
     #
     # public methods
@@ -143,8 +173,10 @@ class MLforKidsImageProject:
     # Returns a prediction for the image at the specified location
     def prediction(self, image_location: str):
         if hasattr(self, "ml_model") == False:
-            raise RuntimeError("Machine learning model has not been trained for this project")
-        testimg = image.load_img(image_location, target_size=MLforKidsImageProject.IMAGESIZE)
+            raise RuntimeError(
+                "Machine learning model has not been trained for this project")
+        testimg = image.load_img(image_location,
+                                 target_size=MLforKidsImageProject.IMAGESIZE)
         testimg = image.img_to_array(testimg)
         testimg = np.expand_dims(testimg, axis=0)
         predictions = self.ml_model.predict(testimg)
